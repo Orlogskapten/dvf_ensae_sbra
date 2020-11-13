9d9e0dfd54efd900978d05de5e5cda36afaf6c40
Wenceslas Sanchez <51153777+Orlogskapten@users.noreply.github.com>
2020-11-13 09:21:50 +0100

    Bokeh serveur script

--- script/bokeh_server/dashboard/test.py
+++ script/bokeh_server/dashboard/test.py
@@ -3,16 +3,18 @@ from tornado.ioloop import IOLoop
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+import json
 
 from bokeh.application.handlers import FunctionHandler
 from bokeh.application import Application
-from bokeh.models import (GeoJSONDataSource, HoverTool, Div, CustomJS, Select
-, LinearColorMapper, ColorBar, ColumnDataSource, Button)
+from bokeh.models import (GeoJSONDataSource, HoverTool, CustomJS, Select
+, LinearColorMapper, ColorBar, ColumnDataSource, Button, Slider, TapTool, RadioButtonGroup)
 from bokeh.palettes import RdYlGn
 from bokeh.plotting import figure
 from bokeh.server.server import Server
 from bokeh.layouts import (column, row)
-
+from bokeh.tile_providers import (get_provider, Vendors)
+from bokeh.models.tickers import FixedTicker
 
 base_crs= {"init": "epsg:4326"}
 
@@ -45,7 +47,6 @@ paris_arr= paris_mutation.merge(arrondissement, on= "c_ar")
 paris_arr["valeur_metre_carre"]= paris_arr["valeurfonc"] / paris_arr["sbati"]
 
 # Set up our data CRS
-base_crs= {"init": "epsg:4326"}
 paris_mutation.crs= base_crs
 arrondissement.crs= base_crs
 paris_arr.crs= base_crs
@@ -65,6 +66,26 @@ menage_per_arr_mean= menage_per_arr_mean.merge(menage_per_arr_count, on= "c_ar")
 menage_per_arr_mean["geometry"]= menage_per_arr_mean["c_ar"].map(arrondissement_geometry)
 menage_per_arr_mean= gpd.GeoDataFrame(menage_per_arr_mean)
 menage_per_arr_mean.crs= base_crs
+
+menage_per_arr_mean= menage_per_arr_mean.to_crs({'init': 'epsg:3395'}) # mercator projection
+
+# Ordering arrondissement to make easier the manipulation on CustomJS
+menage_per_arr_mean["c_ar"]= menage_per_arr_mean["c_ar"].astype(int)
+menage_per_arr_mean.sort_values(by= "c_ar", ascending= True, inplace= True)
+menage_per_arr_mean["c_ar"]= menage_per_arr_mean["c_ar"].astype(str)
+menage_per_arr_mean.reset_index(inplace= True, drop= True)
+
+def to_date_col(df, col= "datemut", formated= "%Y-%m-%d"):
+    return pd.to_datetime(df[col], format= "%Y-%m-%d")
+
+
+paris_arr["datemut"]= to_date_col(paris_arr)
+paris_arr["mois"]= paris_arr["datemut"].apply(lambda x: x.month)
+paris_arr["annee"]= paris_arr["datemut"].apply(lambda x: x.year)
+
+month_paris_arr= paris_arr[var_arr_col+["mois"]].groupby(["mois"], as_index= False).mean()
+year_paris_arr= paris_arr[var_arr_col+["annee"]].groupby(["annee"], as_index= False).mean()
+
 
 var_base= "valeur_metre_carre"
 
@@ -95,14 +116,19 @@ def modify_doc(doc):
                          , location= (0, 0)
                          )
 
-    p= figure(title= "Paris", plot_height= 500, plot_width= 800
-               , toolbar_location= "below")
+    tools= "pan,wheel_zoom,poly_select,box_select,tap"
+    p= figure(title= "Paris", plot_height= 500, plot_width= 800, toolbar_location= "below"
+              , x_axis_type="linear", y_axis_type="linear", tools= tools)
+    tile_provider= get_provider(Vendors.CARTODBPOSITRON_RETINA)
+    p.add_tile(tile_provider)
 
     # Define the left histogram / barplot
-    plot= figure(title= "Densité", plot_height= 500, plot_width= 500
+    plot= figure(title= "Densité", plot_height= 200, plot_width= 500
                   , toolbar_location= None)
+    plot_annee= figure(title="Densité arr", plot_height=200, plot_width=500
+                  , toolbar_location=None)
 
-    # Selec for map figure
+    # Select for map figure
     select= Select(title="Sélectionner la variable", options= var_arr_col, value= "valeur_metre_carre")
 
     # Reset plot and map localisation in the figure
@@ -114,13 +140,45 @@ def modify_doc(doc):
     button= Button(label= 'Reset')
     button.js_on_click(callback3_test)
 
+    # Select month or year for line plot
+    radio_button= RadioButtonGroup(labels= ["Année", "Mois"], active= 0)
+
+    # Select bins with slider
+    num_bins= 20
+    slider= Slider(start= 10, end= 100, value= num_bins,
+                    step= 10, title='Nombre de bins')
+
     def update_map(attr, old, new):
-        var_base= select.value
+        # Get the new value of our selectors
+        var_base= select.value # Select
+        num_bins= slider.value # Slider
+        active_but = radio_button.active # Button month / year
 
         # Update data
-        our_new_data= gpd.GeoDataFrame(menage_per_arr_mean[["geometry"]+var_arr_col+["counts"]])
+        our_new_data= gpd.GeoDataFrame(menage_per_arr_mean[["geometry"]+var_arr_col+["counts"]+base_arr_col])
         our_new_data.crs= base_crs
         geo_arr_json.geojson= our_new_data.to_json()
+
+        # Update hist data
+        hist_data = paris_arr[var_base].apply(lambda x: np.log(x) if x > 0 else x)
+        hist, edges= np.histogram(hist_data[np.isfinite(hist_data)], bins= num_bins)
+        hist_df = pd.DataFrame({var_base: hist,
+                                "left": edges[:-1],
+                                "right": edges[1:]})
+        hist_df["interval"] = ["%d to %d" % (left, right) \
+                               for left, right in zip(hist_df["left"], hist_df["right"])]
+        # print(hist_df)
+        src_hist.data= dict(ColumnDataSource(hist_df).data)
+
+        if active_but == 0:
+            line_data.data= dict(ColumnDataSource(year_paris_arr).data)
+            let_go = plot_annee.line(source=line_data, x="annee", y= var_base)
+        else:
+            line_data.data= dict(ColumnDataSource(month_paris_arr).data)
+            let_go = plot_annee.line(source=line_data, x="mois", y= var_base)
+            plot_annee.xaxis.major_label_overrides= {1:"Janvier", 2:"Fevrier", 3:"Mars", 4:"Avril", 5:"Mai", 6:"Juin"
+                                                         , 7:"Juillet", 8:"Aout", 9:"Septembre", 10:"Octobre"
+                                                         , 11:"Novembre", 12:"Decembre"}
 
         # Update color bar
         color_mapper.high= our_new_data[var_base].max()
@@ -129,29 +187,85 @@ def modify_doc(doc):
         # Update color change in map
         fill_color["field"]= var_base
         fill_color["transform"]= color_mapper
-        # Dont know how to update patches property
+        # Dont know how to update patches property (and hist)
+        # update map
         arr= p.patches(source= geo_arr_json
                         , fill_color= fill_color
                         , line_color= "black"
+                        , fill_alpha= 0.8
                         )
+
+        # Update histogram
+        plot_hist = plot.quad(source=src_hist, left="left", right="right", top=var_base, bottom=0)
+
         pass
+
+    def update_bins(attr, old, new):
+        num_bins = slider.value  # Slider
+        var_base = select.value  # Select
+
+        # Update hist data
+        hist_data = paris_arr[var_base].apply(lambda x: np.log(x) if x > 0 else x)
+        hist, edges = np.histogram(hist_data[np.isfinite(hist_data)], bins=num_bins)
+        hist_df = pd.DataFrame({var_base: hist,
+                                "left": edges[:-1],
+                                "right": edges[1:]})
+        hist_df["interval"] = ["%d to %d" % (left, right) \
+                               for left, right in zip(hist_df["left"], hist_df["right"])]
+        # print(hist_df)
+        src_hist.data = dict(ColumnDataSource(hist_df).data)
+
+        # Update histogram
+        plot_hist = plot.quad(source=src_hist, left="left", right="right", top=var_base, bottom=0)
+        pass
+
+    def update_line_select(attr, old, new):
+        active_but= radio_button.active
+        var_base= select.value
+
+        if active_but == 0:
+            line_data.data= dict(ColumnDataSource(year_paris_arr).data)
+            let_go = plot_annee.line(source=line_data, x="annee", y= var_base)
+        else:
+            line_data.data= dict(ColumnDataSource(month_paris_arr).data)
+            let_go = plot_annee.line(source=line_data, x="mois", y= var_base)
+            plot_annee.xaxis.major_label_overrides= {1:"Janvier", 2:"Fevrier", 3:"Mars", 4:"Avril", 5:"Mai", 6:"Juin"
+                                                         , 7:"Juillet", 8:"Aout", 9:"Septembre", 10:"Octobre"
+                                                         , 11:"Novembre", 12:"Decembre"}
+        pass
+
+
 
     # Map figure
     arr= p.patches(source= geo_arr_json
                     , fill_color= fill_color
                     , line_color= "black"
+                    , fill_alpha= 0.8
                     )
 
-    # Histogram figure
-    hist_data= paris_arr["pp"].apply(lambda x: np.log(x) if x > 0 else x)
-    hist, edges= np.histogram(hist_data, bins= 10)
-    plot.quad(top= hist, bottom= 0, left= edges[:-1], right= edges[1:]
-              , alpha= 0.4)
 
+    # Histogram figure
+    hist_data = paris_arr[var_base].apply(lambda x: np.log(x) if x > 0 else x)
+    hist, edges = np.histogram(hist_data[np.isfinite(hist_data)], bins=num_bins)
+    hist_df = pd.DataFrame({var_base: hist,
+                            "left": edges[:-1],
+                            "right": edges[1:]})
+    hist_df["interval"] = ["%d to %d" % (left, right) \
+                           for left, right in zip(hist_df["left"], hist_df["right"])]
+    src_hist = ColumnDataSource(hist_df)
+    plot_hist = plot.quad(source=src_hist, left="left", right="right", top=var_base, bottom=0)
+
+    # Plot line per year : month
+    line_base_var= var_base
+    year_or_month= "annee"
+    line_data= ColumnDataSource(year_paris_arr)
+    let_go= plot_annee.line(source= line_data, x= year_or_month, y= var_base)
+    plot_annee.xaxis.major_label_orientation = 3.14 / 4
 
     # Map hover
     p.add_tools(HoverTool(tooltips= [("Arrondissement", "@c_ar")
                                      , ("OK", "OK")]))
+
 
     # Adjust our grid (delete line, ticks etc.)
     p.xgrid.grid_line_color = None
@@ -164,15 +278,48 @@ def modify_doc(doc):
     p.yaxis.major_label_text_font_size = '0pt'
 
 
-
-
     # Legend color bar
     p.add_layout(color_bar, "below")
 
     # Update our data
+    fake_data = ColumnDataSource({"indices": []})
+    codes = """
+    var index_selected = source.selected.indices;
+    fake_data.data= {new: index_selected};
+    fake_data.change.emit();
+    console.log(index_selected)
+    console.log(fake_data);
+    """
+    callback= CustomJS(args= dict(source= geo_arr_json, fake_data= fake_data), code= codes)
+    p.js_on_event("tap", callback)
+
     select.on_change("value", update_map)
-    layout= column(column(row(select, width= 400), p), button)
-    doc.add_root(row(layout, plot))
+    slider.on_change("value", update_bins)
+    radio_button.on_change("active", update_line_select)
+
+    # layout= column(column(row(select, width= 400), p), button)
+    layout= column(
+        column(
+            row(
+                select, width= 400
+            )
+            , row(
+                row(
+                    p, width= 800
+                )
+                , column(
+                        plot
+                        , slider
+                        , plot_annee
+                        , row(radio_button, width= 400)
+                )
+            ), sizing_mode="scale_width"
+        )
+        , row(
+            button, width= 50
+        )
+    )
+    doc.add_root(layout)
 
     # doc.add_root(p)
     pass
@@ -195,3 +342,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
